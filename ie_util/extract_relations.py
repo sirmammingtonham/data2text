2981cdcf0acdb33904c0844ef57922eb52c44c53
sirmammingtonham <ethanj217@gmail.com>
2021-11-10 23:46:36 -0500

    move submodule

--- ie_util/extract_relations.py
+++ ie_util/extract_relations.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import json
 import torch
 from tqdm import tqdm
@@ -11,7 +11,7 @@ sentence_encoder = opennre.encoder.TransformerEntityEncoder(
 	mask_entity=False
 )
 
-rel2id = json.load(open('./data/ie/rotowire_rel2id.json'))
+rel2id = json.load(open('../data/ie/rotowire_rel2id.json'))
 model = opennre.model.SoftmaxNN(sentence_encoder, len(rel2id), rel2id)
 model.load_state_dict(torch.load('./OpenNRE/ckpt/none_roberta-base_entity.pth.tar')['state_dict'])
 model.to(torch.device('cuda:0'))
@@ -23,8 +23,9 @@ def extract(dataset):
 	return dataset
 
 if __name__ == '__main__':
-	_, test = save_tuples_for_extraction("./data/rotowire", "./data/bart")
+	model_name = sys.argv[1]
+	_, test = save_tuples_for_extraction(data_dir="../data/rotowire", summary_dir=f"../data/{model_name}")
 	extracted_tuples = extract(test)
 
-	with open(os.path.join('./data/bart', 'test_tuples.txt'), 'w+') as f:
+	with open(os.path.join(f'../data/{model_name}', f'{model_name}_test_tuples.txt'), 'w+') as f:
 	    f.writelines([json.dumps(x) + '\n' for x in extracted_tuples])
