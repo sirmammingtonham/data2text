dff16e9a232a09297e69e61ac636940cedd26f44
sirmammingtonham <ethanj217@gmail.com>
2021-11-12 00:24:01 -0500

    hide onmt

--- eval/onmt/train/init.lua
+++
@@ -1,8 +0,0 @@
-local train = {}
-
-train.Checkpoint = require('onmt.train.Checkpoint')
-train.EpochState = require('onmt.train.EpochState')
-train.Optim = require('onmt.train.Optim')
-train.Greedy = require('onmt.train.Greedy')
-
-return train
